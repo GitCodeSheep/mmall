9874303191c2123437dc3cdecc537996d79ce499
GitCodeSheep <473328659@qq.com>
2019-03-28 16:15:53 +0800

    secound commit

---
+++ src/main/java/com/mmall/pojo/Cart.java
@@ -0,0 +1,68 @@
+package com.mmall.pojo;
+
+import java.util.Date;
+
+public class Cart extends CartKey {
+    private Integer productId;
+
+    private Integer quantity;
+
+    private Integer checked;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Cart(Integer id, Integer userId, Integer productId, Integer quantity, Integer checked, Date createTime, Date updateTime) {
+        super(id, userId);
+        this.productId = productId;
+        this.quantity = quantity;
+        this.checked = checked;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    public Cart() {
+        super();
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getChecked() {
+        return checked;
+    }
+
+    public void setChecked(Integer checked) {
+        this.checked = checked;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
